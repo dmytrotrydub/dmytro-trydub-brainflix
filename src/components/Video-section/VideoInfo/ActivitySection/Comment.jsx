ee84a515768327361888b6912a73855de0999ea9
dmytrotrydub <tridub.d87@gmail.com>
2022-05-09 21:16:02 -0400

    Comments section mobile done

--- src/components/Video-section/VideoInfo/ActivitySection/Comment.jsx
+++ src/components/Video-section/VideoInfo/ActivitySection/Comment.jsx
@@ -1,18 +1,15 @@
 import "./_Comment.scss";
 
 const Comment = (props) => {
-  console.log(props);
   return (
     <div className='comment-card'>
-      <div className='comment-card__avatar-holder'>
-        <img src='' alt='commenter' />
-      </div>
+      <div className='comment-card__avatar-holder'></div>
       <div className='comment-card__comment-container'>
         <div className='name-date'>
           <p className='name-date__name'>{props.name}</p>
           <p className='name-date__date'>{props.date}</p>
         </div>
-        <p className='comment-card__text'>{props.content}</p>
+        <p className='comment-card__text'>{props.comment}</p>
       </div>
     </div>
   );
--- src/components/Video-section/VideoInfo/PostedComments.jsx
+++ src/components/Video-section/VideoInfo/PostedComments.jsx
@@ -1,19 +1,26 @@
 import "./_PostedComment.scss";
 import Comment from "../VideoInfo/ActivitySection/Comment";
-import { render } from "@testing-library/react";
 
 const PostedComments = (props) => {
   return (
     // ! for each comments create a comment card with name date and title
     <div className='posted-comments'>
-      {props.postedComments.forEach((comment, i) => {
-        <Comment
-          key={i}
-          name={comment.name}
-          comment={comment.comment}
-          date={comment.timestamp}
-        />;
-        // console.log(comment.name);
+      {props.postedComments.map((comment, i) => {
+        console.log(comment);
+        const date = new Date(comment.timestamp);
+        const day = String(date.getDate()).padStart(2, 0);
+        const month = String(date.getMonth() + 1).padStart(2, 0);
+        const year = date.getFullYear();
+        const fullDate = `${month}/${day}/${year}`;
+
+        return (
+          <Comment
+            key={i}
+            name={comment.name}
+            comment={comment.comment}
+            date={fullDate}
+          />
+        );
       })}
     </div>
   );
